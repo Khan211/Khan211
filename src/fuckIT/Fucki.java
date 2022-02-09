0f619e7ec14045f803952d263f35ca2f66024418
Khan211 <37129084+Khan211@users.noreply.github.com>
2022-02-09 01:43:07 -0500

    Update Fucki.java
    Cool

--- src/fuckIT/Fucki.java
+++ src/fuckIT/Fucki.java
@@ -5,6 +5,7 @@ public class Fucki {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("fuck this shit");
+		System.out.println("fuck it");
 
 	}
 
